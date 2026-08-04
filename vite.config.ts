3c9c7a720006e3f532555e3d02d295fafd12b009
faridfirda <faridfirdaus0127@gmail.com>
2026-08-04 10:29:20 +0700

    fix build vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // <-- Tambahkan baris ini
+    open: true,
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 })
